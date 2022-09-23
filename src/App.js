17d6c44cc23a8aef6225c2433e501e3768955f55
Pedro Antonini <p.antoninigomes@gmail.com>
2022-09-23 15:30:07 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import TituloPrincipal from "./Componentes/TituloPrincipal";
 
-function App() {
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TituloPrincipal titulo="clique em mim"/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Componentes/TituloPrincipal.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useState } from "react";
+
+function TituloPrincipal(props){
+  const [titulo, setTitulo] = useState(props.titulo ?? "Ola mundo");
+
+  function clickCallBack(event){
+    alert("Obrigado por clicar")
+  }
+  
+  function handleChangeTitle(event){
+    setTitulo(event.target.value);
+  }
+
+  return (
+    <>
+      <h1 onClick={clickCallBack}>{titulo}</h1>
+
+      <input type="text" onChange={handleChangeTitle}/>
+
+    </>
+  )
+}
+
+export default TituloPrincipal;
